fc17cbf86f93fafdd2aa2978bf6579c3246ad71f
Thom van den Broek <thombroek1606@hotmail.com>
2020-06-03 20:39:27 +0200

    fixed icons on web

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import AccessIcon from './icons/access.svg'
-import AfterEffectsIcon from './icons/aftereffects.svg'
-import ExcelIcon from './icons/excel.svg'
-import FigmaIcon from './icons/figma.svg'
-import IllustratorIcon from './icons/illustrator.svg'
-import PhotoshopIcon from './icons/photoshop.svg'
-import PowerpointIcon from './icons/powerpoint.svg'
-import SketchIcon from './icons/sketch.svg'
-import WordIcon from './icons/word.svg'
+import { ReactComponent as AccessIcon } from './icons/access.svg'
+import { ReactComponent as AfterEffectsIcon } from './icons/aftereffects.svg'
+import { ReactComponent as ExcelIcon } from './icons/excel.svg'
+import { ReactComponent as FigmaIcon } from './icons/figma.svg'
+import { ReactComponent as IllustratorIcon } from './icons/illustrator.svg'
+import { ReactComponent as PhotoshopIcon } from './icons/photoshop.svg'
+import { ReactComponent as PowerpointIcon } from './icons/powerpoint.svg'
+import { ReactComponent as SketchIcon } from './icons/sketch.svg'
+import { ReactComponent as WordIcon } from './icons/word.svg'
 
 export {
   AccessIcon,
